d2cdee55486a0a93abdc3b133c61721e1bc1962c
Allan C. M. Lira <acmlira@gmail.com>
2022-07-06 12:14:49 -0300

    Remove unused file

--- internal/filter/card.go
+++
@@ -1,9 +0,0 @@
-package filter
-
-// func cardMap() map[string][]string {
-// 	var m = make(map[string][]string, 10)
-// 	m["code"] = codes
-// 	m["value"] = values
-// 	m["suit"] = suits
-// 	return m
-// }
